4a214b9fbdcc76d5e78d9ebd3f6efb4921d6d196
RetrO_o <54993680+aushwin@users.noreply.github.com>
2020-04-09 11:02:53 +0530

    Optimized Sieve
    Did some optimization

--- src/Algos/sievePrime.kt
+++ src/Algos/sievePrime.kt
@@ -1,36 +1,29 @@
 package Algos
 
-import Tips.Reader
-
-
 fun findSieve(n:Int): Array<Int> {
-    val ar = Array<Int>(n){0}
-    for (i in 3 until n+1 step 2)
-        ar[i] = 1
-    var i=3
-    var j=0
-    while(i<n){
-        if(ar[i]==1){
-            j=i*i
-            while(j<n){
-                ar[j]=0
-                j+=i;
-            }
+    val arr = Array<Int>(n){0}
+    println(arr.contentToString()) // fordebugging
+    for(i in 3 until n step 2)
+        arr[i]=1
+    for(i in 0 until n step 2 ){
+        if(arr[i]==1){
+
+            for(j in i.times(i) until n)
+                arr[j]=0
         }
-     i+=2
     }
-    ar[0]=0
-    ar[1]=0
-    ar[2]=1
-    return ar
+    println(arr.contentToString()) // fordebugging
+    arr[2]==1
+    return arr
 }
 
+
 fun main(args:Array<String>){
-    val sc = Reader()
-    println("Enter the limit")
-    val n = sc.nextInt()
+    val n = readLine()!!.toInt()
     val ar = findSieve(n)
-    for(i in ar.indices)
-        if(ar[i]==1)
-        print("$i ")
+    println(ar.contentToString())
+    ar.forEachIndexed{index,it->
+        if(it==1)
+            print("$index ")
+    }
 }
